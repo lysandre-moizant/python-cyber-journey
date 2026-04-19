f7ffc2b5a67190ace7ba3cd1ecba9ff799014465
Lysandre MOIZANT <moizantlysandre0@gmail.com>
2026-04-19 16:58:18 +0200

    jour2: input() et conversion de types int()

--- mois-01-fondations/jour2-input.py
+++ mois-01-fondations/jour2-input.py
@@ -3,6 +3,12 @@ nom_machine = input("Nom de la machine : ")
 ip = input("Adresse IP : ")
 port = int(input("Port SSH : "))
 
+#Ce que je dois retenir --> input() renvoie toujours du texte
+#Donc port vaut "22" (texte) et je veux faire ( texte+ nombre) cela ne fonctionne pas comme cela python ne peut pas le faire 
+# la solution : int() --> port = int(input("Port SSH : "))
+#En effet int() transforme "22" en 22 --> du texte en nombre entier ! 
+#Résultat attendu 22+1 = 23 car port est maintenant un vrai nombre 
+
 print(f"\n--- Rapport ---")
 print(f"Machine : {nom_machine}")
 print(f"IP : {ip}")
